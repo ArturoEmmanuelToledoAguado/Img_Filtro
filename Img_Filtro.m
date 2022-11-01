a8f24c57403b9db2a43a0cf8caea951229401993
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-01 15:34:50 -0600

    Fin prototipo 1

--- Img_Filtro.m
+++ Img_Filtro.m
@@ -17,10 +17,16 @@ imgRUm=imnoise(img,'speckle');
 
 %%FILTROS DE MATLAB
 %Filtro de la media (promediador='average')
-media=fspecial('average',[4,4])
+media=fspecial('average',[4,4]);%Matriz 4x4
 imgFMg=imfilter(imgRG,media);
 imgFMsp=imfilter(imgRSP,media);
 imgFMum=imfilter(imgRUm,media);
+
+%Filtro gaussiano
+gauss=fspecial('gaussian',[4,4]);
+imgFGg=imfilter(imgRG,media);
+imgFGsp=imfilter(imgRSP,media);
+imgFGum=imfilter(imgRUm,media);
 
 %Mostramos imagenes
 figure
@@ -34,6 +40,9 @@ title('Ruido Gaussiano')
 subplot(4,3,5)
 imshow(imgFMg);
 title('Filtro Media 4x4')
+subplot(4,3,6)
+imshow(imgFGg);
+title('Filtro Gaussiano 4x4')
 
 subplot(4,3,7)
 imshow(imgRSP)
@@ -41,6 +50,9 @@ title('Ruido Impulsivo')
 subplot(4,3,8)
 imshow(imgFMsp)
 title('Filtro Media 4x4')
+subplot(4,3,9)
+imshow(imgFGsp)
+title('Filtro Gaussiano 4x4')
 
 subplot(4,3,10)
 imshow(imgRUm)
@@ -48,3 +60,6 @@ title('Ruido Multiplicativo')
 subplot(4,3,11)
 imshow(imgFMum)
 title('Filtro Media 4x4')
+subplot(4,3,12)
+imshow(imgFGum)
+title('Filtro Gaussiano 4x4')
