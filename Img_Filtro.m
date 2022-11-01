862266c49b1e4913ed10a3758a87f6b1fcc07b79
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-01 15:22:19 -0600

    Filtro Mat Media

--- Img_Filtro.m
+++ Img_Filtro.m
@@ -17,6 +17,10 @@ imgRUm=imnoise(img,'speckle');
 
 %%FILTROS DE MATLAB
 %Filtro de la media (promediador='average')
+media=fspecial('average',[4,4])
+imgFMg=imfilter(imgRG,media);
+imgFMsp=imfilter(imgRSP,media);
+imgFMum=imfilter(imgRUm,media);
 
 %Mostramos imagenes
 figure
@@ -27,11 +31,20 @@ title('Original')
 subplot(4,3,4)
 imshow(imgRG)
 title('Ruido Gaussiano')
+subplot(4,3,5)
+imshow(imgFMg);
+title('Filtro Media 4x4')
 
 subplot(4,3,7)
 imshow(imgRSP)
 title('Ruido Impulsivo')
+subplot(4,3,8)
+imshow(imgFMsp)
+title('Filtro Media 4x4')
 
 subplot(4,3,10)
 imshow(imgRUm)
 title('Ruido Multiplicativo')
+subplot(4,3,11)
+imshow(imgFMum)
+title('Filtro Media 4x4')
