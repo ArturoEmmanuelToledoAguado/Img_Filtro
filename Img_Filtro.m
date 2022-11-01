ecd36a8099516547ae8260b7ba47646fc9040640
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-01 15:13:47 -0600

    Mostramos imagenes

--- Img_Filtro.m
+++ Img_Filtro.m
@@ -5,6 +5,7 @@ close all;
 %Abrir la imagen
 img=imread('IMG/1.jpg');
 
+%%RUIDOS
 %Ruido Gaussiano
 imgRG=imnoise(img,'gaussian');
 
@@ -14,4 +15,23 @@ imgRSP=imnoise(img,'salt & pepper');
 %Ruido Uniforme (multiplicativo)
 imgRUm=imnoise(img,'speckle');
 
-%M
+%%FILTROS DE MATLAB
+%Filtro de la media (promediador='average')
+
+%Mostramos imagenes
+figure
+subplot(4,3,1)
+imshow(img)
+title('Original')
+
+subplot(4,3,4)
+imshow(imgRG)
+title('Ruido Gaussiano')
+
+subplot(4,3,7)
+imshow(imgRSP)
+title('Ruido Impulsivo')
+
+subplot(4,3,10)
+imshow(imgRUm)
+title('Ruido Multiplicativo')
