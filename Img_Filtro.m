50059e2cd95c1b4f17e7b109f50b7e4277e4f600
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-01 16:56:15 -0600

    Media Propuesta

--- Img_Filtro.m
+++ Img_Filtro.m
@@ -21,45 +21,86 @@ media=fspecial('average',[4,4]);%Matriz 4x4
 imgFMg=imfilter(imgRG,media);
 imgFMsp=imfilter(imgRSP,media);
 imgFMum=imfilter(imgRUm,media);
-
 %Filtro gaussiano
-gauss=fspecial('gaussian',[4,4]);
+gauss=fspecial('gaussian',[4,4]);%Matriz 4x4
 imgFGg=imfilter(imgRG,media);
 imgFGsp=imfilter(imgRSP,media);
 imgFGum=imfilter(imgRUm,media);
 
 %Mostramos imagenes
 figure
-subplot(4,3,1)
 imshow(img)
 title('Original')
 
-subplot(4,3,4)
+figure
+subplot(3,3,1)
 imshow(imgRG)
 title('Ruido Gaussiano')
-subplot(4,3,5)
+subplot(3,3,2)
 imshow(imgFMg);
 title('Filtro Media 4x4')
-subplot(4,3,6)
+subplot(3,3,3)
 imshow(imgFGg);
 title('Filtro Gaussiano 4x4')
 
-subplot(4,3,7)
+subplot(3,3,4)
 imshow(imgRSP)
 title('Ruido Impulsivo')
-subplot(4,3,8)
+subplot(3,3,5)
 imshow(imgFMsp)
 title('Filtro Media 4x4')
-subplot(4,3,9)
+subplot(3,3,6)
 imshow(imgFGsp)
 title('Filtro Gaussiano 4x4')
 
-subplot(4,3,10)
+subplot(3,3,7)
 imshow(imgRUm)
 title('Ruido Multiplicativo')
-subplot(4,3,11)
+subplot(3,3,8)
 imshow(imgFMum)
 title('Filtro Media 4x4')
-subplot(4,3,12)
+subplot(3,3,9)
 imshow(imgFGum)
 title('Filtro Gaussiano 4x4')
+
+%%FILTROS PROPUESTOS
+    %Filtro de la media
+% for i=1:4
+%     for j=1:4
+%         med(i,j)=randi([1 3],1,1);
+%     end
+% end
+med=[1.5,1,1,1.5;1,1,1,1;1,1,1,1;1.5,1,1,1.5];
+m=sum(sum(med));
+for i=1:4
+    for j=1:4
+        med(i,j)=1/m;
+    end
+end
+%Aplicacion de la mascara
+imgFMPg=imfilter(imgRG,med);
+imgFMPsp=imfilter(imgRSP,med);
+imgFMPum=imfilter(imgRUm,med);
+
+%Mostramos las imagenes
+figure
+subplot(3,3,1)
+imshow(imgRG)
+title('Ruido Gaussiano')
+subplot(3,3,2)
+imshow(imgFMPg)
+title('Filtro Media Propuesto')
+
+subplot(3,3,4)
+imshow(imgRSP)
+title('Ruido Impulsivo')
+subplot(3,3,5)
+imshow(imgFMPsp)
+title('Filtro Media Propuesto')
+
+subplot(3,3,7)
+imshow(imgRSP)
+title('Ruido Impulsivo')
+subplot(3,3,8)
+imshow(imgFMPum)
+title('Filtro Media Propuesto')
